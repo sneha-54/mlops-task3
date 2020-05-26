841fbfccf630dfe9e7d986ee5d74cc70e56bcd21
sneha-54 <65108965+sneha-54@users.noreply.github.com>
2020-05-27 02:19:28 +0530

    Add files via upload
    jenkins job outputs and files

---
+++ mail.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+host_address="*****@gmail.com"
+host_pass="**********"
+guest_address="*****@gmail.com"
+subject="Status of Success of your model"
+content="congo!! your model has been trained successfully"
+message = MIMEMultipart()
+message['From']=host_address
+message['To']=guest_address
+message['Subject']=subject
+message.attach(MIMEText(content,'plain'))
+session= smtplib.SMTP('smtp.gmail.com',587)
+session.starttls()
+session.login(host_address,host_pass)
+text=message.as_string()
+session.sendmail(host_address,guest_address,text)
+session.quit()
+print("Mail sent successfully")
